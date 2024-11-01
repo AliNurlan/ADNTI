27227fbbd9002248790c5055b0b3ff39dbe07553
AliNurlan <149326775+AliNurlan@users.noreply.github.com>
2024-11-01 12:26:58 +0500

    Forms
    здесь написано как работать с формами и с адресами

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,re_path,include
 
 urlpatterns = [
+    
     re_path(r'^sum\d+', include('data.urls')),
     re_path(r'^about', include('data.urls')),
     path('admin/', admin.site.urls),
--- Backend/data/urls.py
+++ Backend/data/urls.py
@@ -1,9 +1,26 @@
-from django.urls import path,re_path
+from django.urls import path,re_path,include
 from . import views
 
+product_patterns = [
+    path("", views.products),
+    path("comments", views.comments),
+    path("questions", views.questions),
+]
+
 urlpatterns=[
+    path("products/<int:id>/", include(product_patterns)),
+    re_path(r"^user/(?P<name>\D+)/(?P<age>\d+)", views.user),
+    re_path(r"^user/(?P<name>\D+)", views.user),
+    re_path(r"^user", views.user),
     path('sum/<str:num>', views.sum),
     re_path(r'^about', views.about,kwargs={"name":"Tom", "age": 38}),
+    path("set", views.set),
+    path("get", views.get),
+    path('json_response/',views.json_response),
+    path('json/',views.json),
+    path('b/',views.index2),
     path('',views.index),
+    path('connection',views.connection),
+    path('access',views.access),
     path('a',views.a),
 ]
